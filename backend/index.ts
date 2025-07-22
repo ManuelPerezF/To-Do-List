75dc1f6657749c846d73253662fe6310e501a5dc
ManuelPerezF <A01741957@tec.mx>
2025-07-22 03:41:07 -0700

    cambios cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import taskRoutes from './src/routes/taskRoutes';
+import cors from 'cors';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', taskRoutes)
 
